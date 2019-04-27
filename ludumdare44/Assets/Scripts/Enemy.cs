15963f715c16de3f2cbe9156370df047ceb73886
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2019-04-27 12:01:18 -0600

    first commit with walkers along a path

---
+++ ludumdare44/Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using Dreamteck.Splines;
+using UnityEngine;
+
+namespace Tofunaut.LudumDare44
+{
+    public class Enemy : MonoBehaviour
+    {
+        public int health;
+        public float pathSpeed;
+
+        private SplineFollower _splineFollower;
+
+        private void Start()
+        {
+            _splineFollower = GetComponent<SplineFollower>();
+            _splineFollower.computer = FindObjectOfType<SplineComputer>();
+            _splineFollower.followSpeed = pathSpeed;
+        }
+    }
+}
---
+++ ludumdare44/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Tofunaut.LudumDare44
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        public Enemy enemyPrefab;
+        public float spawnEventInterval;
+        public float spawnChance;
+        public Transform spawnPosition;
+
+        private Coroutine _spawnCoroutine;
+
+        private void OnEnable()
+        {
+            if(_spawnCoroutine != null)
+            {
+                StopCoroutine(_spawnCoroutine);
+            }
+
+            _spawnCoroutine = StartCoroutine(Spawn());
+        }
+
+        private IEnumerator Spawn()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(spawnEventInterval);
+
+                TrySpawnEnemy();
+            }
+        }
+
+        private void TrySpawnEnemy()
+        {
+            Debug.Log("try spawn");
+            float roll = Random.value;
+            if (roll < spawnChance)
+            {
+                SpawnEnemy();
+            }
+        }
+
+        private void SpawnEnemy()
+        {
+            Debug.Log("spawned!");
+            Enemy enemy = Instantiate(enemyPrefab);
+            enemy.transform.position = spawnPosition.position;
+        }
+    }
+}
